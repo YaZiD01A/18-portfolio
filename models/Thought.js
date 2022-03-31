6f4aa935d5d970c4cfce7c2324f961b38d46ff7d
Yazid <yalhujery01@gmail.com>
2022-03-30 22:05:47 -0400

    last push

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,42 @@
 const {Schema, model} = require ("mongoose")
+const reactionSchema = require ("./Reaction")
 
+const thoughtInfo = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            require: true,
+            minlength: 1,
+            maxlength: 280
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+
+
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        reactions: [reactionSchema]
+    },
+
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+
+thoughtInfo.virtual ("reactionCount").get (()=> {
+    return this.reactions.length
+})
+
+const Thought = model ("Thought", thoughtInfo)
+
+
+module.exports = Thought
